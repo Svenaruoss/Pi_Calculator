6b671bcd6d15d7a620f2b3025ba29acf25af4f19
Svenja123 <svenja.ruoss@juventus.schule>
2024-10-13 12:27:30 +0200

    Alles von altem Code ruebercopiert

--- src/main.c
+++ src/main.c
@@ -1,50 +1,159 @@
 /********************************************************************************************* */
-//    Eduboard2 ESP32-S3 Template with BSP
-//    Author: Martin Burger
+//    PI Calculator
+//    Author: Svenja Ruoss
 //    Juventus Technikerschule
 //    Version: 1.0.0
 //    
-//    This is the ideal starting point for a new Project. BSP for most of the Eduboard2
-//    Hardware is included under components/eduboard2.
-//    Hardware support can be activated/deactivated in components/eduboard2/eduboard2_config.h
+//   
+//    
+//   
 /********************************************************************************************* */
-#include "eduboard2.h"
-#include "memon.h"
+#include                        "eduboard2.h"
+#include                        "memon.h"
 
-#include "math.h"
+#include                        "math.h"
 
-#define TAG "TEMPLATE"
+#define TAG                     "PI_Calculator"
+
+#define SteuerTask              "SteuerTask"
+#define LeibnizTask             "LeibnizTask"   
+#define NilakanthaTask          "OtherTask"
 
 #define UPDATETIME_MS 100
 
-void templateTask(void* param) {
-    //Init stuff here
+//----------------- Switch -----------------------------------------------------------------------------
+#define ControllingMenu
+#define ButtonON
 
-    for(;;) {
-        // task main loop
-        if(button_get_state(SW0, true) == SHORT_PRESSED) {
-            led_toggle(LED0);
+//----------------- Global Variable --------------------------------------------------------------------
+double LeibnizPi                = 0.0;
+double NilakanthaPI             = 0.0;
+unsigned char ChangeVariable    = 0;    //0 für Leibniz, 1 für Nilakantha
+
+//----------------- EventBits --------------------------------------------------------------------------
+#define BIT_change      1 << 0
+#define BIT_Start       1 << 1
+#define BIT_End         1 << 2
+#define BIT_SetBack     1 << 3
+
+EventGroupHandle_t xControlleventgroup;
+//----------------- Steuer-Task ------------------------------------------------------------------------
+
+void Steuer_Task(void* param){
+
+     TickType_t xStartTime = xTaskGetTickCount();
+     lcdFillScreen(BLACK);
+
+    if(button_get_state(SW0, true) == SHORT_PRESSED){
+        xEventGroupSetBits(xControlleventgroup, BIT_Start);
+        xEventGroupClearBits(xControlleventgroup, BIT_End);
+    }
+
+    if(button_get_state(SW1, true) == SHORT_PRESSED){
+        xEventGroupSetBits(xControlleventgroup, BIT_End);
+        xEventGroupClearBits(xControlleventgroup, BIT_Start);
+    }
+    if(button_get_state(SW2, true) == SHORT_PRESSED){
+        xEventGroupSetBits(xControlleventgroup, BIT_SetBack);
+    }
+    if(button_get_state(SW3, true) == SHORT_PRESSED){
+        if(xEventGroupGetBits(xControlleventgroup) & BIT_change) {
+            xEventGroupClearBits(xControlleventgroup, BIT_change);
         }
-        led_toggle(LED7);
-        // delay
-        vTaskDelay(UPDATETIME_MS/portTICK_PERIOD_MS);
+        if(xEventGroupGetBits(xControlleventgroup) & !BIT_change) {
+            xEventGroupSetBits(xControlleventgroup, BIT_change);
+        }
+    }
+
+//----------------- Zeitmessung -------------------------------------------------------------------------------
+    if (fabs(LeibnizPi * 4 - 3.14159) < 0.00001) {                                              
+        TickType_t xEndTime = xTaskGetTickCount();
+        printf("Zeit bis 5 Stellen: %ld ms\n", (xEndTime - xStartTime) * portTICK_PERIOD_MS);
+    }
+
+    if (fabs(NilakanthaPI * 4 - 3.14159) < 0.00001) {
+        TickType_t xEndTime = xTaskGetTickCount();
+        printf("Zeit bis 5 Stellen: %ld ms\n", (xEndTime - xStartTime) * portTICK_PERIOD_MS);
+    }
+    
+    vTaskDelay(500/portTICK_PERIOD_MS);
+}
+//----------------- Leibniz-Calcalator ------------------------------------------------------------------------
+
+void Leibniz_Calculator(void* param){
+
+    if(ChangeVariable == 0){
+        int k = 0;
+        while (1) {
+            EventBits_t uxBits = xEventGroupWaitBits(xControlleventgroup, BIT_Start | BIT_End | BIT_SetBack, pdTRUE, pdFALSE, portMAX_DELAY);
+
+            if (uxBits & BIT_Start) {
+                LeibnizPi += (k % 2 == 0 ? 1.0 : -1.0) / (2 * k + 1);
+                k++;
+                printf("Leibniz π: %.10f\n", LeibnizPi * 4);
+                vTaskDelay(pdMS_TO_TICKS(500));
+                ESP_LOGI(TAG, "%lf", LeibnizPi);
+            } 
+            else if (uxBits & BIT_End) {
+                vTaskSuspend(NULL);
+            } else if (uxBits & BIT_SetBack) {
+                LeibnizPi = 0.0;
+                k = 0;
+            }
+        }
     }
 }
 
+//----------------- Nilakantha-Calclator ----------------------------------------------------------------------
+void Nilakantha_Calculator(void* param){
+
+    if(ChangeVariable == 1){
+        int k = 1;
+        while (1) {
+            EventBits_t uxBits = xEventGroupWaitBits(xControlleventgroup, BIT_Start | BIT_End | BIT_SetBack, pdTRUE, pdFALSE, portMAX_DELAY);
+            if (uxBits & BIT_Start) {
+                NilakanthaPI += (k % 2 == 0 ? -4.0 : 4.0) / ((2 * k) * (2 * k + 1) * (2 * k + 2));
+                k++;
+                printf("Nilakantha π: %.10f\n", NilakanthaPI);
+                vTaskDelay(pdMS_TO_TICKS(500));
+            } else if (uxBits & BIT_End) {
+                vTaskSuspend(NULL);
+            } else if (uxBits & BIT_SetBack) {
+                NilakanthaPI = 3.0;
+                k = 1;
+            }
+        }
+    }
+}
+
+//----------------- Task's ------------------------------------------------------------------------
 void app_main()
 {
+
+    lcdFillScreen(BLACK);
+    xControlleventgroup = xEventGroupCreate();
     //Initialize Eduboard2 BSP
     eduboard2_init();
     
     //Create templateTask
-    xTaskCreate(templateTask,   //Subroutine
-                "testTask",     //Name
-                2*2048,         //Stacksize
-                NULL,           //Parameters
-                10,             //Priority
-                NULL);          //Taskhandle
-    for(;;) {
-        vTaskDelay(2000/portTICK_PERIOD_MS);
-        ESP_LOGI(TAG, "Hello Eduboard");
-    }
+    xTaskCreate(Steuer_Task,            //Subroutine
+                "Steuerung",            //Name
+                2*2048,                 //Stacksize
+                NULL,                   //Parameters
+                9,                      //Priority
+                NULL);                  //Taskhandle
+
+    xTaskCreate(Leibniz_Calculator,     //Subroutine
+                "Leibniz-Rechner",      //Name
+                2*2048,                 //Stacksize
+                NULL,                   //Parameters
+                1,                      //Priority
+                NULL);                  //Taskhandle
+
+    xTaskCreate(Nilakantha_Calculator,  //Subroutine
+                "Nilakantha-Rechner",   //Name
+                2*2048,                 //Stacksize
+                NULL,                   //Parameters
+                1,                      //Priority
+                NULL);                  //Taskhandle
 }
